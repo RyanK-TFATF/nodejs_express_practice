53d31e208e0d642d80e73938935dfe0daf190a2d
Ryan Kelley <92961892+RyanK-TFATF@users.noreply.github.com>
2022-11-10 15:09:50 -0500

    querystring #2

--- Codecademy.com_Lessons/HTTP/queryString/apps.js
+++ Codecademy.com_Lessons/HTTP/queryString/apps.js
@@ -1,2 +1,7 @@
 const querystring = require('querystring');
 const url = 'https://www.example.com/p/a/t/h?course=node&lesson=http';
+
+const split = url.split('?');
+const queryToParse = split[1];
+
+console.log(queryToParse); 
