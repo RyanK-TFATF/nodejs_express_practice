fb42b10888eecfb987c9ce1ab04dc364ada18083
Ryan Kelley <92961892+RyanK-TFATF@users.noreply.github.com>
2022-11-11 10:39:28 -0500

    Using Queries -- Solution Used

--- Codecademy.com_Lessons/Express.js/usingQueries/app.js
+++ Codecademy.com_Lessons/Express.js/usingQueries/app.js
@@ -1,3 +1,15 @@
+/*
+Use req.query to update the proper element in the expressions array.
+We’ve imported a helper function from /utils.js to help with this task.
+You can use the updateElement() helper function in your PUT /expressions/:id route.
+It takes three arguments:
+    id (the ID number of the element)
+    queryArguments (the new, updated expression object from req.query)
+    elementList (the array which contains the element to update)
+updateElement() updates that specific element in the elementList array (you’ll pass in the expressions array), and then returns the updated element.
+Be sure to check that an expression with the id you provide exists in the expressions array (getIndexById() can help)!
+*/
+
 const express = require('express');
 const app = express();
 
@@ -5,7 +17,7 @@ const app = express();
 app.use(express.static('public'));
 
 const { getElementById, getIndexById, updateElement,
-        seedElements } = require('./utils');
+  seedElements } = require('./utils');
 
 const expressions = [];
 seedElements(expressions, 'expressions');
@@ -25,8 +37,15 @@ app.get('/expressions/:id', (req, res, next) => {
   }
 });
 
+// Provided Solution Used :( 
 app.put('/expressions/:id', (req, res, next) => {
-  
+  const expressionIndex = getIndexById(req.params.id, expressions);
+  if (expressionIndex !== -1) {
+    updateElement(req.params.id, req.query, expressions);
+    res.send(expressions[expressionIndex]);
+  } else {
+    res.status(404).send();
+  }
 });
 
 app.listen(PORT, () => {
