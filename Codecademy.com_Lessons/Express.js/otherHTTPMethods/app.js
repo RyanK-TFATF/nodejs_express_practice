9122b9701ee24a948f2a21e6ad59c3a3d9e7ae86
Ryan Kelley <92961892+RyanK-TFATF@users.noreply.github.com>
2022-11-11 10:28:00 -0500

    Other HTTP Methods Lesson

---
+++ Codecademy.com_Lessons/Express.js/otherHTTPMethods/app.js
@@ -0,0 +1,39 @@
+/*
+For now, open a PUT /expressions/:id route with an empty (req, res, next) callback function. 
+We will fully implement its functionality in the next exercise.
+*/
+
+const express = require('express');
+const app = express();
+
+// Serves Express Yourself website
+app.use(express.static('public'));
+
+const { getElementById, seedElements } = require('./utils');
+
+const expressions = [];
+seedElements(expressions, 'expressions');
+
+const PORT = process.env.PORT || 4001;
+
+app.get('/expressions', (req, res, next) => {
+  res.send(expressions);
+});
+
+app.get('/expressions/:id', (req, res, next) => {
+  const foundExpression = getElementById(req.params.id, expressions);
+  if (foundExpression) {
+    res.send(foundExpression);
+  } else {
+    res.status(404).send();
+  }
+});
+
+// My Code Here 
+app.put('/expressions/:id', (req, res, next) => {
+
+});
+
+app.listen(PORT, () => {
+  console.log(`Listening on port ${PORT}`);
+});
