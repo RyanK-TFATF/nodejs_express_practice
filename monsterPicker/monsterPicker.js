ccfc995e5ccd23abdeca89c3eb0eb31432dd571a
Ryan Kelley <92961892+RyanK-TFATF@users.noreply.github.com>
2022-11-11 22:08:05 -0500

    Monster Picker v0.1a
    Added requirements for HTTP and Express modules.

--- monsterPicker/monsterPicker.js
+++ monsterPicker/monsterPicker.js
@@ -1,1 +1,12 @@
 // D&D Monster Picker -- Create, Read, Update, Delete Monsters and Associated Images 
+// v0.1a
+
+// Requirements for Express
+const express = require('express');
+const app = express(); 
+
+// Requirements for HTTP 
+const http = require('http');
+
+
+
