c838079cc3f283aa7f65245ea6fc1fe451ced249
Ryan Kelley <92961892+RyanK-TFATF@users.noreply.github.com>
2022-11-11 22:34:34 -0500

    Monster Picker v0.1e

--- monsterPicker/monsterPicker.js
+++ monsterPicker/monsterPicker.js
@@ -1,5 +1,5 @@
 // D&D Monster Picker -- Create, Read, Update, Delete Monsters and Associated Images 
-// v0.1d
+// v0.1e
 
 // Requirements for Express
 const express = require('express');
@@ -9,16 +9,40 @@ const app = express();
 const http = require('http');
 
 // Empty Array for Monsters
-const monsters = []; 
-
+const monsterArr = []; 
 
 // Create (POST) -- handlePostRequest
+app.post('/monsters', (req, res, next) => {
+    const newMonster = createElement('monsterArr', req.query);
+    if (newMonster) {
+      monsterArr.push(newMonster);
+      res.status(201).send(newMonster);
+    } else {
+      res.status(400).send();
+    }
+  });
 
-// Read (GET) -- handleGetRequest
-app.get('/monsters', req, res, next) => {
+// Read (GET) -- handleGetRequest by id 
+app.get('/monsters/:id', (req, res, next) => {
+    const foundMonster = getElementById(req.params.id, monsterArr);
+    if (foundMonster) {
+      res.send(foundMonster);
+    } else {
+      res.status(404).send();
+    }
+  });
 
-}
-// Update (PUT) -- handlePutRequest
+// Update (PUT) -- handlePutRequest by ID 
+app.put('/monsters/:id', (req, res, next) => {
+    const monsterIndex = getIndexById(req.params.id, monsterArr);
+    if (monsterIndex !== -1) {
+      updateElement(req.params.id, req.query, expressions);
+      res.send(monsterArr[monsterIndex]);
+    } else {
+      res.status(404).send();
+    }
+  });
+
 
 // Delete (DELETE) -- handleDeleteRequest
 
