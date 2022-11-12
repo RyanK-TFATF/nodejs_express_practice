0f8bfa6ed6e3af878b3da9de0c8f4d1a6c4dc928
Ryan Kelley <92961892+RyanK-TFATF@users.noreply.github.com>
2022-11-11 22:45:27 -0500

    Deleted Garbage, Starting Fresh

--- monsterPicker/monsterPicker.js
+++ monsterPicker/monsterPicker.js
@@ -1,55 +1,19 @@
 // D&D Monster Picker -- Create, Read, Update, Delete Monsters and Associated Images 
-// v0.1e
+// v0.2
 
 // Requirements for Express
 const express = require('express');
 const app = express(); 
+const port = 4000
 
-// Requirements for HTTP 
-const http = require('http');
 
-// Empty Array for Monsters
-const monsterArr = []; 
 
 // Create (POST) -- handlePostRequest
-app.post('/monsters', (req, res, next) => {
-    const newMonster = createElement('monsterArr', req.query);
-    if (newMonster) {
-      monsterArr.push(newMonster);
-      res.status(201).send(newMonster);
-    } else {
-      res.status(400).send();
-    }
-  });
 
 // Read (GET) -- handleGetRequest by id 
-app.get('/monsters/:id', (req, res, next) => {
-    const foundMonster = getElementById(req.params.id, monsterArr);
-    if (foundMonster) {
-      res.send(foundMonster);
-    } else {
-      res.status(404).send();
-    }
-  });
 
 // Update (PUT) -- handlePutRequest by ID 
-app.put('/monsters/:id', (req, res, next) => {
-    const monsterIndex = getIndexById(req.params.id, monsterArr);
-    if (monsterIndex !== -1) {
-      updateElement(req.params.id, req.query, expressions);
-      res.send(monsterArr[monsterIndex]);
-    } else {
-      res.status(404).send();
-    }
-  });
-
 
 // Delete (DELETE) -- handleDeleteRequest
 
 // Start Server on 4002
-const server = http.createServer((req, res) => {
-    res.end('Monster Picker HTTP Server Operational');
-  });  
-  server.listen(4002, () => {
-    
-  });
