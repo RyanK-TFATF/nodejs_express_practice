15ef8dae7c0dcd65bebaa37b8c782d7bcac9bab4
Ryan Kelley <92961892+RyanK-TFATF@users.noreply.github.com>
2022-11-26 21:59:31 -0500

    Updates

--- notes/vanillaJS/arrayPractice/arrayPractice.js
+++ notes/vanillaJS/arrayPractice/arrayPractice.js
@@ -1,11 +1,7 @@
 // Array Practice -- Ryan Kelley 
-
 const intArr = [-4, -3, -2, -1, 0, 1, 2, 3, 4];
-const stringArr = ['Red', 'Green', 'Blue', 'Yellow', 'Orange'];
 const floatArr  = [-1.0, -0.5, 0.0, 0.5, 1.0];
 const boolArr = [true, false, true, true, false, false];
-
-// Array Methods -- .map(), .filter(), .forEach()
 
 // .filter() will create a new array based on results of function test passed to the method. 
 function isEven(num) {
@@ -17,32 +13,21 @@ function isEven(num) {
 }
 
 const evenArr = intArr.filter(isEven);
-console.log(evenArr);
+// console.log(evenArr);
 
 // .map() creates a new array by calling the specified function on all non-empty elements. 
 function squareIt(num) {
     return num * num; 
-
 }
 const squaredFloatArr = floatArr.map(squareIt);
-console.log(squaredFloatArr);
+// console.log(squaredFloatArr);
 
-/* .forEach() calls a function for each non-empty element in an array. 
-function inBed(str) {
-    newString = str + 'in bed.'; 
-    return newString;
+// String Array 
+const stringArr = ['Red', 'Green', 'Blue', 'Yellow', 'Orange'];
+// .forEach() calls a function for each non-empty element in an array. 
+function isAColor(str) {
+    newString = str + ' is a color.'; 
+    console.log(newString);
 }
-var newStringArr = stringArr.forEach(inBed);
-console.log(stringArr.forEach(inBed));
-console.log(newStringArr.toString()); 
 
-function noU(bool) {
-    if (bool === true) {
-        return false;
-    } else {
-        return true;
-    }
-}
-console.log(boolArr.forEach(noU));
-console.log(boolArr.forEach);
-*/ 
+console.log(stringArr.forEach(isAColor)); 
---
+++ notes/vanillaJS/ternaryOperator/ternaryOperator.js
@@ -0,0 +1,9 @@
+let x = 0; 
+
+if (x > 0) {
+    console.log("X is greater than 0.");
+} else if (x < 0) {
+    console.log("X is less than 0.");
+} else {
+    console.log("X is 0.");
+}
